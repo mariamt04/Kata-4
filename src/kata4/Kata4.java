83f9a90998d003ce4bd470587fcb824de1621e0a
mariamt04 <>
2016-11-04 11:45:18 +0000

    Kata 4 v3 muestra histograma leyendo dominios de un fichero

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,22 +1,17 @@
 package kata4;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Kata4 {
 
-    public static void main(String[] args) {
-        File file = new File("c:\\pub");
-        print(file.listFiles(), "");
-    }
-    
-    private static void print (File[] files, String indent){
-        if(files == null) return;
-        for (File file : files) {
-            System.out.println(indent + 
-                    (file.isDirectory() ? "+" : "-") +
-                    file.getName());
-            if(!file.isDirectory() || file.isHidden()) continue;
-            print(file.listFiles(),  indent + "  ");
-        }
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String fileName = "emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(fileName);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
 }
